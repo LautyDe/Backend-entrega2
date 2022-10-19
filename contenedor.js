36419fffb004b236b2388d13ac250138c777f5a6
Lautaro Demonte <lauty.d.p@gmail.com>
2022-10-18 21:29:19 -0300

    exporto el Contenedor y lo llevo al index

--- contenedor.js
+++ contenedor.js
@@ -166,9 +166,4 @@ class Contenedor {
     }
 }
 
-const productos = new Contenedor("./productos.json");
-productos.save({ title: "prueba", price: 150 });
-productos.getById(2);
-productos.getAll();
-productos.deleteById(4);
-productos.deleteAll();
+module.exports = Contenedor;
